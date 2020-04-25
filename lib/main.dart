026cef1cdad99ef415cd225f26b882539bbe94f5
Peter Ekhator <peterekhator@gmail.com>
2020-04-25 05:06:39 -0400

    micard

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(MiCard());
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(height: 100.0, width: 100.0, color: Colors.yellow),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.green,
+                )
+              ],
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+            )
+          ],
+        ),
       ),
-    ),
-  );
+    ));
+  }
 }
